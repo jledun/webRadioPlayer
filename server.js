f0321ebfab897f6732c544b389c5470deb48efc1
Julien Ledun <j.ledun@iosystems.fr>
2018-08-03 15:37:11 +0200

    mplayer joue !
    Signed-off-by: Julien Ledun <j.ledun@iosystems.fr>

--- server.js
+++ server.js
@@ -5,16 +5,26 @@ const path = require('path');
 
 const options = {
   cli: [
+    '-slave',
     // '-loop 0',
-    '-ao alsa'
+    // '-ao pulse'
   ]
 };
 const url = require(path.resolve('./').concat('/radioStreams.json'));
+let rt = [];
+let status = "";
 
 const mp = spawn("mplayer", options.cli.concat(url[0].url));
 
 mp.stdout.on('data', data => {
-  console.log(data.toString());
+  const tmp = data.toString().trim();
+  if (tmp.substr(0, 2) === 'A:') {
+    status = tmp;
+  }else{
+    rt = rt.concat((tmp.split("\n").length > 0) ? tmp.split("\n") : [tmp]);
+  }
+  console.log(rt);
+  console.log(status);
 });
 mp.stderr.on('data', data => {
   console.log('error', data.toString());
